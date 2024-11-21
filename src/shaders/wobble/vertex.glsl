ffc0ad8c05cc8f5b1bc1864fedf0dbdb5c2109d8
bumbeishvili <me@davidb.dev>
2024-11-21 21:09:28 +0400

    simple wobble

--- src/shaders/wobble/vertex.glsl
+++ src/shaders/wobble/vertex.glsl
@@ -1,7 +1,12 @@
+
+#include ../includes/simplexNoise4d.glsl
 varying vec2 vUv;
 void main() {
     //csm_Position.y += sin(csm_Position.z * 3.0) * 0.5;
 
-    vUv = uv;
+    //vUv = uv;
+
+    float wobble = simplexNoise4d(vec4(csm_Position.xyz, 0.0));
+    csm_Position += wobble * normal;
 
 }
